319f69e8c8ec83c8258c3045636ec0efeeec9433
Mohammad Akbar <akbarintheframe@gmail.com>
2026-07-02 22:15:48 +0530

    docs: add file header with author, date, and project description
    Added comments and documentation for LED blink program.

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,3 +1,11 @@
+/**
+ * Author: [Your Name]
+ * Date: July 2, 2026
+ * Description: An advanced LED blink program that uses a potentiometer 
+ *              connected to pin A0 to control the blinking speed of an LED 
+ *              on pin 13. It also logs the total blink count to the Serial Monitor.
+ */
+
 const int ledPin = 13;
 const int potPin = A0;
 int blinkCount = 0;
@@ -8,7 +16,6 @@ void setup() {
 }
 
 void loop() {
-  // Read the potentiometer value (0 to 1023) to use as the delay in milliseconds
   int blinkDelay = analogRead(potPin);
   
   digitalWrite(ledPin, HIGH);
