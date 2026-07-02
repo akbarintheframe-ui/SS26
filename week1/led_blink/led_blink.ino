05ed6cb684a57b84b918b51018709f297ea50f18
Mohammad Akbar <akbarintheframe@gmail.com>
2026-07-02 22:14:35 +0530

    feat: add serial logging for blink count
    Added blink count tracking and serial output.

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,7 +1,9 @@
 const int ledPin = 13;
+int blinkCount = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -9,4 +11,8 @@ void loop() {
   delay(500);
   digitalWrite(ledPin, LOW);
   delay(500);
+  
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
