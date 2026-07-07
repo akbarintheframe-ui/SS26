0d4d9e161cdda8d3a095e191f46dc7a7f4cfb5d1
Mohammad Akbar <akbarintheframe@gmail.com>
2026-07-07 13:08:41 +0530

    Create config.h for API and WiFi settings
    Added configuration file for WiFi credentials and OpenWeatherMap API.

---
+++ assignment/Q35. REST API – Weather Data Fetcher/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi Credentials
+const char* ssid = "OnePlus Nord 5 F35A";
+const char* password = "aasansapass";
+
+// OpenWeatherMap API Configuration
+// Sign up at openweathermap.org to get your free API Key
+const char* apiKey = "953ee5f9313debc38319a78be7955e86";
+const char* city = "Jammu";
+const char* countryCode = "IN"; 
+
+#endif
